72712a2d8ce354dd636885b65ac28e5d5511665a
Victor Shermon <victor804.gt@gmail.com>
2019-09-08 22:18:37 -0300

    navegacao prev e next final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-import Slide from './slide.js';
+import {SlideNav} from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-slide.changeSlide(4);
-slide.activePrevSlide();
+// slide.changeSlide(4);
+// slide.activePrevSlide();
+slide.addArow('.prev', '.next');
+console.log(slide);
